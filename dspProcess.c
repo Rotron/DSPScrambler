bf7d533b40e786df84e8c134e79c4905a51e8f3c
deborahbobbio <deborahbobbio@gmail.com>
2013-08-11 11:12:00 -0400

    Update dspProcess.c

--- dspProcess.c
+++ dspProcess.c
@@ -1,70 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "dsk6713_aic23.h" //codec support
+#define N 129
 
 #include "dspProcess.h"
 
-const int BL = 187;
-const short B[187] = {
-        6,     14,     27,     45,     67,     90,    108,    118,    116,
-       97,     62,     14,    -40,    -93,   -134,   -156,   -155,   -130,
-      -88,    -38,      7,     36,     43,     25,    -13,    -59,   -100,
-     -125,   -124,    -96,    -48,      7,     54,     79,     73,     37,
-      -20,    -82,   -130,   -147,   -128,    -74,      0,     74,    125,
-      136,    101,     29,    -63,   -145,   -194,   -190,   -132,    -32,
-       82,    176,    220,    196,    107,    -25,   -164,   -265,   -296,
-     -239,   -106,     70,    239,    346,    355,    251,     56,   -179,
-     -385,   -495,   -463,   -282,     11,    339,    604,    715,    611,
-      289,   -190,   -707,  -1109,  -1242,   -989,   -307,    761,   2081,
-     3455,   4656,   5474,   5764,   5474,   4656,   3455,   2081,    761,
-     -307,   -989,  -1242,  -1109,   -707,   -190,    289,    611,    715,
-      604,    339,     11,   -282,   -463,   -495,   -385,   -179,     56,
-      251,    355,    346,    239,     70,   -106,   -239,   -296,   -265,
-     -164,    -25,    107,    196,    220,    176,     82,    -32,   -132,
-     -190,   -194,   -145,    -63,     29,    101,    136,    125,     74,
-        0,    -74,   -128,   -147,   -130,    -82,    -20,     37,     73,
-       79,     54,      7,    -48,    -96,   -124,   -125,   -100,    -59,
-      -13,     25,     43,     36,      7,    -38,    -88,   -130,   -155,
-     -156,   -134,    -93,    -40,     14,     62,     97,    116,    118,
-      108,     90,     67,     45,     27,     14,      6
+const int hL = N;
+const short h[N] = {
+       16,    -20,     -4,      6,    -20,     11,      2,    -22,     26,
+      -12,    -16,     36,    -33,      3,     35,    -53,     34,     15,
+      -61,     69,    -25,    -45,     93,    -77,      0,     89,   -124,
+       70,     45,   -143,    148,    -41,   -111,    202,   -153,    -18,
+      200,   -257,    128,    116,   -307,    294,    -58,   -257,    425,
+     -296,    -77,    451,   -544,    237,    306,   -716,    654,    -70,
+     -701,   1109,   -742,   -339,   1521,  -1903,    799,   1808,  -5141,
+     7927,  23757,   7927,  -5141,   1808,    799,  -1903,   1521,   -339,
+     -742,   1109,   -701,    -70,    654,   -716,    306,    237,   -544,
+      451,    -77,   -296,    425,   -257,    -58,    294,   -307,    116,
+      128,   -257,    200,    -18,   -153,    202,   -111,    -41,    148,
+     -143,     45,     70,   -124,     89,      0,    -77,     93,    -45,
+      -25,     69,    -61,     15,     34,    -53,     35,      3,    -33,
+       36,    -16,    -12,     26,    -22,      2,     11,    -20,      6,
+       -4,    -20,     16
 };
 
-int loopindex = 0; //table index
-int bufindex = 0; //buffer index
-short sine_table[LOOPLENGTH]={0,707,1000,707,0,-707,-1000,-707};
-int out_buffer[BUFFERLENGTH]; //output buffer
-short gain = 10;
-interrupt void c_int11() //interrupt service routine
-short out_sample;
-out_sample = sine_table[loopindex++]*gain;
-output_left_sample(out_sample); //output sample value
-out_buffer[bufindex++] = out_sample; //store in buffer
-if (loopindex >= LOOPLENGTH) loopindex = 0; //check end table
-if (bufindex >= BUFFERLENGTH) bufindex = 0; //check end buffer
-return;
-} //return from interrupt
-void main()
-{
-  comm_intr(); //initialise DSK
-    }
+const int sineL = N;
+const short sine[3]
+ = {0,28377,-28377};
 
-// implements an fir filter using fir coefficients defined in fdacoefs.h
-// in fixed-point
-short scramble(buffer *xn){
+short fir(buffer *xn){
 	int j;
 	int yn = 0;
+	int yn1 = 0;
+	int conv1 = 0;
+	int conv2 = 0;
 	// performs the convolution of xn with B
-	for(j=0; j < BL; j++){
-		conv1 += readn(xn,j)*B[j];
+	for(j=0; j < hL; j++){
+		yn += readn(xn,j)*h[j];
 	}
-    
-    for(j=0; j < BL; j++){
-		conv2 += readn(xn,j)*sine_table[j];
-	}
-    
-    yn = (conv1*conv2) * conv1;
-    
+//    	for(j=0; j < sineL; j++){
+//		conv2 += readn(xn,j)*sine[j];
+//	}
+//    	yn = (conv1 * conv2)/(2*N);
 	yn = (yn >> 15) & 0xffff; //converts from Q30 to Q15 fixed point
 	return (short)yn; // must cast to a 16-bit short for output to ALSA
 }
@@ -72,15 +49,24 @@ short scramble(buffer *xn){
 // core dsp block processing
 int dspBlockProcess(short *outputBuffer, short *inputBuffer, buffer *xnL, buffer *xnR, int samples, int * filter_on, double * volume){
 	int i;
+	short sine_val=0;
+	int count = 0;
+	int index = 0;
+	int wn = 0;
 	if(*filter_on == 0) {
 		memcpy((char *)outputBuffer, (char *)inputBuffer, 2*samples); // passthru
 	}
 	else if(*filter_on == 1) {
 		for (i=0; i < samples; i+=2){
 			push(xnL,inputBuffer[i]); // stores the most recent sample in the circular buffer xn
-			push(xnR,inputBuffer[i+1]);
-			outputBuffer[i] = (short)(*volume*scramble(xnL)); // filters the input and stores it in the left output channel
-			outputBuffer[i] = (short)(*volume*scramble(xnR)); // zeros out the right output channel
+			index = count % 3;
+			sine_val = sine[index];
+			count++;
+			wn = (sine_val*fir(xnL));
+			wn = (wn >> 15) & 0xffff;
+			push(xnR,(short)wn);
+			outputBuffer[i] = (short)(*volume*fir(xnR));  // filters the input and stores it in the left output channel
+			outputBuffer[i+1] = 0; // zeros out the right output channel
 		}	
 	}
 	return DSP_PROCESS_SUCCESS;
